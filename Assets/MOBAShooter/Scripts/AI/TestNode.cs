e50d76186497c3de3578485f2db0ee855a1b0a2e
expee105 <experian.elitiawan@gmail.com>
2018-07-23 21:18:58 +0700

    Add TestNode

---
+++ Assets/MOBAShooter/Scripts/AI/TestNode.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestNode : Node
+{
+	public delegate bool NodeTest();
+	private NodeTest _testMethod;
+
+	public TestNode(Node tgtNode, NodeTest testMethod)
+	{
+		_testMethod = testMethod;
+		childs = new List<Node>
+		{
+			tgtNode
+		};
+		state = NodeState.FAILED;
+	}
+
+	public override NodeState Evaluate()
+	{
+		if(_testMethod())
+		{
+			state = childs[0].Evaluate();
+			return state;
+		}
+		else
+		{
+			state = NodeState.FAILED;
+			return state;
+		}
+	}
+}
