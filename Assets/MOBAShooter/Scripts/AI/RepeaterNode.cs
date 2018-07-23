e90b4923a3369af2c739667ae4b9fdfdd03d73e7
expee105 <experian.elitiawan@gmail.com>
2018-07-23 20:57:10 +0700

    Implement RepeaterNode

--- Assets/MOBAShooter/Scripts/AI/RepeaterNode.cs
+++ Assets/MOBAShooter/Scripts/AI/RepeaterNode.cs
@@ -2,9 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RepeaterNode : Node {
+public class RepeaterNode : Node
+{
+	private int _repeatCount;
+
+	public RepeaterNode(Node tgtNode, int maxRepeat)
+	{
+		//Repeater node will always only have 1 child node
+		maxRepeatCount = maxRepeat;
+		_repeatCount = 0;
+		childs = new List<Node>
+		{
+			tgtNode
+		};
+		state = NodeState.FAILED;
+	}
+
     public override NodeState Evaluate()
     {
-        throw new System.NotImplementedException();
+		if(++_repeatCount < maxRepeatCount)
+		{
+			state = childs[0].Evaluate();
+			return state;
+		}
+		else
+		{
+			state = NodeState.FAILED;
+			return state;
+		}
     }
+
+	public int maxRepeatCount { get; private set; }
 }
