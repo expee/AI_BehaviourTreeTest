e37db9cd5a8178a7c74d1afdc5b1e9e25d1c25bc
pickzelz <pickzelz@gmail.com>
2018-07-23 17:42:25 +0700

    - add check point

--- Assets/Scripts/MOBASodor/Engine/CheckPointEngine.cs
+++ Assets/Scripts/MOBASodor/Engine/CheckPointEngine.cs
@@ -10,11 +10,13 @@ public class CheckPointEngine : MonoBehaviour {
     private float TimeRemainingToClaim;
     private bool isRaid;
     private GameManager.Team WhoIsRaiding;
+    private Renderer m_objMaterial;
 
 	// Use this for initialization
 	void Start () {
         TeamOwner = GameManager.Team.E_NONE;
         TimeRemainingToClaim = TimeToClaim;
+        m_objMaterial = GetComponent<Renderer>();
 	}
 	
 	// Update is called once per frame
@@ -44,8 +46,43 @@ public class CheckPointEngine : MonoBehaviour {
                     }
                     TimeRemainingToClaim = TimeToClaim;
                 }
+
+            }
+            if (isChangeOwner)
+            {
+                Color color;
+                switch(TeamOwner)
+                {
+                    case GameManager.Team.E_BLUE:
+                        color = Color.blue;
+
+                        break;
+                    case GameManager.Team.E_RED:
+                        color = Color.red;
+                        break;
+                    default:
+                        color = Color.white;
+                        break;
+                }
+                m_objMaterial.material.SetColor("_Color", color);
+
             }
         }
-
+        
 	}
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Red Team is Raiding");
+        isRaid = true;
+        if(TeamOwner != GameManager.Team.E_RED)
+            WhoIsRaiding = GameManager.Team.E_RED;
+        else if(TeamOwner != GameManager.Team.E_BLUE)
+            WhoIsRaiding = GameManager.Team.E_BLUE;
+    }
+
+    private void OnMouseUp()
+    {
+        isRaid = false;
+    }
 }
